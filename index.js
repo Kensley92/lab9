82738080299e65a13f76530d0d6d22ff43e06c15
Kensley92 <72991374+Kensley92@users.noreply.github.com>
2021-03-26 23:17:41 -0400

    lab9

---
+++ index.js
@@ -0,0 +1,35 @@
+import Express from "express";
+import fs from "fs";
+
+const App = Express();
+const port = 3010;
+
+let fileContents = fs.readFileSync("database.json");
+
+let database = JSON.parse(fileContents);
+
+App.use("/", Express.static("public"));
+App.get("employees/:name", (req, res) => {
+    let result = {"error": "Not available!"};
+
+    database.forEach((value) => {
+        if(req.params.name ==value.name) {
+            result = value;
+        }
+    });
+
+    App.get("ages/:number", (req, res) => {
+        let result = {"error": "Not available!"};
+    
+        database.forEach((value) => {
+            if(req.params.age ==value.age) {
+                result = value;
+            }
+        });
+
+    res.json(result);
+})
+
+App.listen(port, () =>{
+    console.log("Server running!");
+})
